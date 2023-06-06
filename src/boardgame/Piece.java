9d99cc1f69848e53d99e66ad348bebaa04caea41
gab1ru14 <gabriielviictor28@gmail.com>
2023-06-06 00:21:43 -0300

    Starting to implement Board and Piece

---
+++ src/boardgame/Piece.java
@@ -0,0 +1,21 @@
+package boardgame;
+
+public class Piece {
+
+	protected Position position;
+	private Board board;
+	
+	public Piece(Board board) {
+		this.board = board;
+		position = null;
+	}
+
+	protected Board getBoard() { // somente classes do mesmo pacote e subclasses podem acessar o taboleiro
+		return board;
+	}
+
+	
+	
+	
+	
+}
